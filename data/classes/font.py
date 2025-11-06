4daa49cd0114b55111c60e7d31e23e8023f49d6a
gamexdifficulty <gamexdifficulty@yahoo.com>
2025-11-06 18:24:20 +0100

    Renamed letter to fit windows file naming conventions

--- data/classes/font.py
+++ data/classes/font.py
@@ -12,6 +12,10 @@ LETTER_MAP = {
     "smaller_than":"<",
     "greater_than":">",
     "backslash":r"\\",
+    "pipe":"|",
+    "star":"*",
+    "quote":'"',
+    "doubledot":":"
     }
 SPACE_SIZE = 3
 
@@ -34,12 +38,34 @@ class Font:
 
     def draw(self,text:str="",pos:list=[0,0],centered:bool=False):
         w = pos[0]
-        for letter in text:
-            letter = letter.lower()
-            if letter == " ":
-                w += SPACE_SIZE
-            else:
-                if letter not in self.sprite_config:
-                    letter = "error"
-                self.game.window.render(self.sprite_config[letter]["sprite"],[w,pos[1]])
-                w += self.sprite_config[letter]["size"][0]+1
+        if centered:
+            width = 0
+            for letter in text:
+                letter = letter.lower()
+                if letter == " ":
+                    width += SPACE_SIZE
+                else:
+                    if letter not in self.sprite_config:
+                        letter = "error"
+                    width += self.sprite_config[letter]["size"][0]+1
+
+            w = int(pos[0]-width/2)
+            for letter in text:
+                letter = letter.lower()
+                if letter == " ":
+                    w += SPACE_SIZE
+                else:
+                    if letter not in self.sprite_config:
+                        letter = "error"
+                    self.game.window.render(self.sprite_config[letter]["sprite"],[w,pos[1]])
+                    w += self.sprite_config[letter]["size"][0]+1
+        else:
+            for letter in text:
+                letter = letter.lower()
+                if letter == " ":
+                    w += SPACE_SIZE
+                else:
+                    if letter not in self.sprite_config:
+                        letter = "error"
+                    self.game.window.render(self.sprite_config[letter]["sprite"],[w,pos[1]])
+                    w += self.sprite_config[letter]["size"][0]+1
