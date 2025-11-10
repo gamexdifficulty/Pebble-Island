09c0a5feae83a6c6d5afce8f69e363c3e0e71cf3
Sebastian <SebastianTress@t-online.de>
2025-11-10 17:32:10 +0100

    add overlaymanager

---
+++ data/classes/overlay_manager.py
@@ -0,0 +1,26 @@
+class OverlayManager:
+    def __init__(self, game):
+        self.game = game
+        self.overlay = {}
+        self.overlay_current = None
+        self.overlay_active = False
+
+    def update(self):
+        if self.overlay_current != None:
+            self.overlay[self.overlay_current].update()
+
+    def draw(self):
+        if self.overlay_current != None:
+            self.overlay[self.overlay_current].draw()
+
+    def register_overlay(self, overlay_name, overlay):
+        self.overlay[overlay_name] = overlay
+
+    def load_overlay(self, overlay_name):
+        self.overlay_current = overlay_name
+        self.overlay_active = True
+
+    def unload_overlay(self):
+        self.overlay_current = None
+        self.overlay_active = False
+        
